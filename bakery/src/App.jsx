b593d937f60a18f453362678aeab06e2afb237c2
Lesedi <skhosanalesedi888@gmail.com>
2024-04-25 10:08:58 +0200

    big update

--- bakery/src/App.jsx
+++ bakery/src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./Components/HomePage"
 
 function App() {
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        {/* <Route path="/item/:id" element={<ItemView />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ bakery/src/Components/HomePage.jsx
@@ -0,0 +1,12 @@
+import About from "./About";
+
+
+
+function HomePage() {
+    return (
+        <About/>
+        
+    );
+  }
+  
+  export default HomePage;
