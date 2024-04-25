67e2d1c1a35d458779950e6eaf707af835e1a983
Lesedi <skhosanalesedi888@gmail.com>
2024-04-25 13:42:28 +0200

    navbar added

--- bakery/src/Components/About.jsx
+++ bakery/src/Components/About.jsx
@@ -8,9 +8,20 @@ import NavBar from "./NavBar";
 function About() {
     return (
       <>
-      <NavBar />
-      <div className="about">
-      {/* <img src = {background} alt = "background" /> */}
+      <div className="home-container">
+      <NavBar/>
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          {/* <img src={BannerBackground} alt="" /> */}
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            About Us
+          </h1>
+          <p className="primary-text">
+          Welcome to Foodie, your ultimate destination for delicious and effortless dinner recipes! At Foodie, we believe that cooking shouldn't be complicated. Our mission is to empower home cooks of all skill levels to create mouthwatering meals without stress or hassle.
+          </p></div>
+      </div>
       </div>
       </>
     );
--- bakery/src/Components/NavBar.jsx
+++ bakery/src/Components/NavBar.jsx
@@ -1,9 +1,22 @@
-
+import "../CSS/Navbar.css"
 
 
 function NavBar() {
     return (
-      <></>
+      <>
+      <nav>
+        <div className="navbar-logo">
+            <p>Logo</p>
+        </div>
+        <div className="navlinks">
+            <a href="https://github.com/LSkhosana/Bakery/settings">Home</a>
+            <a href="https://github.com/LSkhosana/Bakery/settings">About</a>
+            <a href="https://github.com/LSkhosana/Bakery/settings">Recipes</a>
+            <a href="https://github.com/LSkhosana/Bakery/settings">Contact</a>
+        </div>
+
+      </nav>
+        </>
     );
   }
   
