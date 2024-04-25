f61e657f06d26841d584545a08d843653198e1c0
Lesedi <skhosanalesedi888@gmail.com>
2024-04-25 08:48:53 +0200

    Array update

---
+++ bakery/src/Components/ProductArr.jsx
@@ -0,0 +1,21 @@
+import croissant from "../Assets/croissants.jpg"
+
+const products =[
+    {
+        id:1,
+        name:"croissant",
+        title:"THE LEGENDARY CROISSANT",
+        imageUrl:croissant,
+        ingredients:"flour, sugar, butter, eggs, milk, yeast",
+        steps: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo vestibulum nulla, quis gravida ex placerat vel. Integer sagittis eros nec libero vehicula, non fringilla neque eleifend. Phasellus commodo diam in arcu posuere, quis consectetur ligula consectetur. Sed ultricies ex a felis luctus, vitae efficitur lacus pharetra. Quisque lacinia justo sit amet leo fermentum, sed suscipit leo mattis. Ut nec elit at ipsum tempor scelerisque. Vivamus dictum lorem ut quam luctus, sed convallis leo varius. Sed eget elit ut nisi fermentum ultrices. Nullam nec nulla in libero lobortis molestie nec in libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis ac libero nec magna tincidunt volutpat. Curabitur nec libero id lorem commodo tempor. Mauris in odio eget sem bibendum posuere ac eu nisl. Aliquam erat volutpat. Phasellus feugiat, eros eget tristique vehicula, est risus aliquam lacus, ac suscipit nisl velit at nibh. Sed elementum odio sit amet sapien iaculis, sed vestibulum libero scelerisque. Mi vate risus eros.",
+          ],
+
+
+        
+
+        
+    }
+]
+
+export default products
